a11cd66026491c6ffcba407e6633398479d020b5
wenle <21st.dec@gmail.com>
2018-06-30 19:09:08 +0800

    set port

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const history = require('connect-history-api-fallback')
 const app = express()
+app.set('port', (process.env.PORT || 3000));
 app.use(history())
 
 app.use(express.static(__dirname +'/dist'))
-app.listen(3000)
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+})

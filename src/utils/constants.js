f8bb51bb571321f7bfeaeea5215d0f270bc80c27
wenle <21st.dec@gmail.com>
2018-08-14 15:07:39 +0800

    fix url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// export const TRIBELA_URL = `${API.baseUrl}v1`
-export const TRIBELA_URL = 'http://127.0.0.1:3000/v1'
+export const TRIBELA_URL = `${API.baseUrl}v1`
+// export const TRIBELA_URL = 'http://127.0.0.1:3000/v1'
 // export const TRIBELA_URL = 'http://192.168.1.107:3000/v1'
 // export const STUFF_WAR_URL = 'http://stuffwar.com/'
 export const STUFF_WAR_URL = 'https://guarded-reef-93022.herokuapp.com/'

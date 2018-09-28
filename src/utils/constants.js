03535d0e7b8620b3fb23feaf8ff46fd2bbf25b27
wenle <21st.dec@gmail.com>
2018-09-28 19:16:10 +0800

    fix url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
-// export const TRIBELA_URL = `${API.baseUrl}v1`
+export const TRIBELA_URL = `${API.baseUrl}v1`
 // export const TRIBELA_URL = 'http://api.stuffwar.com/v1'
 // export const TRIBELA_URL = 'http://127.0.0.1:3000/v1'
-export const TRIBELA_URL = 'http://192.168.1.101:3000/v1'
+// export const TRIBELA_URL = 'http://192.168.1.101:3000/v1'
 // export const STUFF_WAR_URL = 'http://stuffwar.com/'
 export const STUFF_WAR_URL = `${API.STUFFWAR_URL}`

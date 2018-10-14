82f7e22a48c972fb39a2c23acd9f4b0bd3512c5f
wenle <21st.dec@gmail.com>
2018-10-15 01:59:12 +0800

    fix url orders

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ app.set('port', (process.env.PORT || 3000))
 // app.use(history())
 
 app.use(express.static(__dirname +'/dist'))
-app.get('*', function(req, res){
-  res.sendFile(path.join(__dirname + '/dist/index.html'))
-})
 app.get('/campaign/:id', function(req,res){
   const userAgent = req.get('User-Agent')
   if (userAgent.startsWith('facebookexternalhit/1.1') || userAgent === 'Facebot' || userAgent.startsWith('Twitterbot')){
@@ -63,7 +60,6 @@ app.get('/campaign/:id', function(req,res){
 })
 
 app.get('/article/:id', function(req,res){
-  console.log(req.params.id)
   const userAgent = req.get('User-Agent')
   const postData = {
     article: {
@@ -116,6 +112,9 @@ app.get('/article/:id', function(req,res){
   
 })
 
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
